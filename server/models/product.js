95e85599ded4e052db5dba0dd9d7d6233eab23c3
Kanha Bhawani <ghanishtsinghal@gmail.com>
2023-08-14 00:08:43 +0530

    Product Schema

--- server/models/product.js
+++ server/models/product.js
@@ -1,4 +1,29 @@
 /*
 product_id
-
+product_name
+product_discription
+product_image
+product_price
+seller_id
 */
+
+
+const mongoose = require('mongoose');
+
+
+const productSchema = mongoose.Schema(
+    {
+        productId: { type:Number, require: true },
+        productName: { type: String, required: true },
+        productImage: {
+            type: "String",
+            required: true,
+            default:
+            "",
+        },
+        productDiscription: { type: String, required: true, default: "New Product" },
+        productPrice: { type: Number , require: true },
+        seller: [{ type: mongoose.Schema.Types.ObjectId, ref: "Seller" }]
+      
+    }
+  );
