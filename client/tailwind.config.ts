6b0748ce687c3918c625372cfbaaf8cf7fa30adf
SachetVijay <81145614+SachetVijay@users.noreply.github.com>
2023-08-14 16:32:44 +0530

    add product added

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,20 +1,39 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#287FF0",
+
+          secondary: "#FFE11B",
+
+          accent: "#FF9F01",
+
+          neutral: "#2b3440",
+
+          "base-100": "#ffffff",
+
+          info: "#3abff8",
+
+          success: "#388E3C",
+
+          warning: "#FB641B",
+
+          error: "#f87272",
+          body: {
+            "background-color": "#e3e6e6",
+          },
+        },
       },
-    },
+    ],
   },
-  plugins: [],
-}
-export default config
+};
+export default config;
