4bd6202bb26eee208bc359a857072d85f44c3a52
SachetVijay <81145614+SachetVijay@users.noreply.github.com>
2023-08-19 01:26:57 +0530

    Redux Setup

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,17 +1,20 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { wrapper } from '../../redux/store'
+// import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'RootKart',
   description: 'Grid 5.0',
 }
 
-export default function RootLayout({ children }) {
+function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="">{children}</body>
     </html>
   )
 }
+
+export default wrapper.withRedux(RootLayout);
