0922db3c0184717cc869c9ffcbd0066b67616756
SachetVijay <81145614+SachetVijay@users.noreply.github.com>
2023-08-19 16:08:23 +0530

    rainbowkit

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,21 +1,58 @@
+"use client";
 import './globals.css'
-// import { wrapper } from '../../redux/store'
-// import { Inter } from 'next/font/google'
 
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+  polygonMumbai,
+} from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+// import { Inter } from 'next/font/google'
 // const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'RootKart',
-  description: 'Grid 5.0',
-}
+const { chains, publicClient } = configureChains(
+  [polygonMumbai],
+  [
+    alchemyProvider({ apiKey: "NcrmX9FX3r9Va-W1PEpdzXv9AmlgHtZ3" }),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  projectId: '0d7bcb2dedf7781ff2cbc9ef29f0a4f6',
+  chains
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
+
+// export const metadata = {
+//   title: 'RootKart',
+//   description: 'Grid 5.0',
+// }
 
 function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="">{children}</body>
+      <body className="">
+        <WagmiConfig config={wagmiConfig}>
+          <RainbowKitProvider chains={chains}>
+            {children}
+          </RainbowKitProvider>
+        </WagmiConfig>
+      </body>
     </html>
   )
 }
 
-// export default wrapper.withRedux(RootLayout);
 export default RootLayout
